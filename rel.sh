4eaed33463da17024a935f9ae34ef3f2f13241d1
dqh-au <david.q.hogan@gmail.com>
2023-05-26 19:00:14 +1000

    Update rel.sh

--- rel.sh
+++ rel.sh
@@ -5,6 +5,6 @@ rm -rf node_modules .yarn* yarn.lock
 TEMP_PKG=$(mktemp)
 jq 'del(.packageManager, .resolutions)' < package.json > "$TEMP_PKG"
 mv "$TEMP_PKG" package.json
-npm cache clean
+npm cache clean --force
 [ -f package-lock ] && rm package-lock.json
 npm install
