0379c19f194881b0c7dd8cdf583ec93272a6700b
dqh-au <david.q.hogan@gmail.com>
2023-06-16 19:50:47 +1000

    Update rel.sh

--- rel.sh
+++ rel.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-rm -rf node_modules .yarn* yarn.lock
+rm -rf node_modules .yarn .pnp.* yarn.lock
 TEMP_PKG=$(mktemp)
 jq 'del(.packageManager, .resolutions)' < package.json > "$TEMP_PKG"
 mv "$TEMP_PKG" package.json
-npm cache clean --force
+npm cache verify
 [ -f package-lock ] && rm package-lock.json
 npm install
 npm shrinkwrap
