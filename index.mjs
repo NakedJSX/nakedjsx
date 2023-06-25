ca35babda180b17341bc33cca8205d3edc569970
dqh-au <david.q.hogan@gmail.com>
2023-06-25 12:47:58 +1000

    Comment update

--- index.mjs
+++ index.mjs
@@ -1,13 +1,21 @@
 #!/usr/bin/env node
 
 //
-// npx is a bit weird.
+// npx behaviour can be counterintuitive when a standalone package name 'nakedjsx' matches
+// a 'bin' script within a dependency installed in a package around the current working
+// directory.
 //
-// running 'npx nakedjsx' seems to invoke the 'nakedjsx' bin within the @nakedjsx/core dep.
-// UNLESS you specify a version or tag, e.g. 'npx nakedjsx@latest' in which case this script 
-// will be invoked.
+// Running 'npx nakedjsx' in this case will run the 'nakedjsx' bin within the @nakedjsx/core dep.
+// UNLESS you specify a version or tag, e.g. 'npx nakedjsx@latest' in which case the the standalone
+// nakedjsx package (this one) will be invoked.
 //
-// Anyway, reign in the chaos by forwarding invocations of this script through to @nakedjsx/core.
+// Anyway, reign in the chaos by forwarding invocations of this script through to the bin script
+// in @nakedjsx/core.
+//
+// In the event that 'npx nakedjsx' is from run outside of a package that does include the 
+// @nakedjsx/core depenency, then the version of @nakedjsx/core bundled with 'npx nakedjsx'
+// will detect and forward the invocation to the version that the project has installed.
+// TODO: This forwarding mechanism should be migrated into this script.
 //
 
 import { createRequire } from 'node:module'
