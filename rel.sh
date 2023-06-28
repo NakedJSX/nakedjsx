5237a57ef2911d5dbac804db05a859e70d4aca2a
David Hogan <david.q.hogan@gmail.com>
2023-06-28 16:00:54 +1000

    Updated release script

--- rel.sh
+++ rel.sh
@@ -1,11 +1,75 @@
 #!/bin/bash
 cd "$(dirname "$0")"
+set -o errexit
+set -o nounset
 
-rm -rf node_modules .yarn .pnp.* yarn.lock
-TEMP_PKG=$(mktemp)
-jq 'del(.packageManager, .resolutions)' < package.json > "$TEMP_PKG"
-mv "$TEMP_PKG" package.json
+VERSION="$1"
+NPM_TAG="$2"
+TEMP_PKG="$(mktemp)"
+
+OTHER_PACKAGES=(../plugin-asset-image ../plugin-asset-prism ../plugin-asset-mdx ../core)
+
+check_package() {
+	echo "Checking @nakedjsx/$(basename $(pwd)) ..."
+	if [[ ! -z $(git status -s . -- ':!rel.sh') ]]
+	#if [[ ! -z $(git status -s) ]]
+	then
+		git status -s
+		echo "Not clean, aborting"
+		exit 1
+	fi
+}
+
+update_package() {
+	TEMP_PKG=$(mktemp)
+	jq "del(.packageManager, .resolutions) |
+			.version=\"$VERSION\" |
+			if .dependencies? then
+				.dependencies |= with_entries(select(.key | startswith(\"@nakedjsx/\")).value = \"$VERSION\")
+			else
+				.
+			end
+		" < package.json > "$TEMP_PKG"
+	mv "$TEMP_PKG" package.json
+	git add package.json
+	TAG="v$VERSION"
+	git commit -m "$TAG"
+	git tag "$TAG"
+	git push origin
+	git push origin "$TAG"
+}
+
+rm -rf node_modules .yarn* .pnp.* yarn.lock package-lock.json
+git checkout npm-shrinkwrap.json
+
+for package in ${OTHER_PACKAGES[*]}
+do
+	pushd "$package" >/dev/null
+	check_package
+	popd >/dev/null
+done
+check_package
+
+# ok, commit and tag
+
+for package in ${OTHER_PACKAGES[*]}
+do
+	pushd "$package" >/dev/null
+	update_package
+	popd >/dev/null
+done
+update_package
+
+# now publish
+
+for package in ${OTHER_PACKAGES[*]}
+do
+	pushd "$package" >/dev/null
+	npm publish --tag $NPM_TAG
+	popd >/dev/null
+done
+
 npm cache verify
-[ -f package-lock ] && rm package-lock.json
 npm install
 npm shrinkwrap
+npm publish --tag $NPM_TAG
